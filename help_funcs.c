7645bcdce46bc21904bb503f8def7ce9c74137ff
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-11-05 13:13:03 +0100

    bubble_sort: sorts function by moving largest value to end of list

--- help_funcs.c
+++ help_funcs.c
@@ -1,11 +1,11 @@
 #include "sort.h"
 
-/*
+/**
  * swap - compares 2 numbers
  * and interchanges them if the first is larger
  * @a: the numbers to be compared
  * @b: the numbers to be compared
- * Return: 0 for success and 1 for fail.
+ * Return: Nothing.
  */
 void swap(int *a, int *b)
 {
