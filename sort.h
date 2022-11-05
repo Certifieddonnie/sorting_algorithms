6d2931b83fbeb453b9cceb88cb58d899d54ec7c8
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-11-05 10:30:50 +0100

    fixed merge errors

--- sort.h
+++ sort.h
@@ -4,13 +4,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-<<<<<<< HEAD
-void print_array(const int *array, size_t size);
-#endif
-=======
 /* Help Functions */
+void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-
 #endif /*SORT_H*/
->>>>>>> Donnie
